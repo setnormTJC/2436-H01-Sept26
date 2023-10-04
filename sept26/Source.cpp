fc7bb7610316dbf47db45ff2c996d17e73ede8c6
Work <Work@MSI>
2023-10-04 14:12:34 -0500

    Where we ended in 2436 on Oct 3

--- sept26/Source.cpp
+++ sept26/Source.cpp
@@ -25,30 +25,66 @@ int sequentialSearch(vector<T> list, T targetValue)
 int binarySearch(std::vector<int> sortedNums, int searchValue)
 {
 	bool found = false;
-	int first = 0;
+	int first = 0; 
 	int last = sortedNums.size() - 1;
-	int middle = (first + last)/2;
-
+	int middle = (first + last)/2; 
+	
+	int comparisonCount = 0; 
+	
+	int whileLoopCount = 0; 
 	while (first <= last and !found)
 	{
+		whileLoopCount++; 
+		cout << "\n\nOn iteration " << whileLoopCount
+			<< " , `first` changed to " << first << endl;
+
+		cout << "\n\nOn iteration " << whileLoopCount
+			<< " , `middle` changed to " << middle << endl;
+
+		cout << "\n\nOn iteration " << whileLoopCount
+			<< " , `last` changed to " << last << endl;
+
+		//second iter: middle = 0; last = 1; first = 0
 		//nums = 11, 22, 33, 44, 55 (searching for 66) 
 		//first while loop iter: middle = (0 + 4) /2 = 2 
-		if (sortedNums[middle] == searchValue) //33 == 66?
+		if (sortedNums[middle] == searchValue) 
 		{
+			//comparisonCount++;
 			found = true;
 		} 
+
+
+
+		//searchValue = 11;0
+
 		else if (sortedNums[middle] > searchValue)
 		{
-			last = middle - 1;//shift last one to the "left" of middle
+			comparisonCount++;
+			last = middle - 1;//shift last 1 to the "left" of middle
+
+
+
+			//last = 2 - 1
+			// last = 1;
 		}
 		else
 		{
-			first = middle + 1;
+			comparisonCount++;
+			first = middle + 1; //shift first 1 position to the "right" of middle
+
+
+
 		}
 	
 		middle = (first + last) / 2; //use ceil() or floor() for odd lengths … be cautious
-	
+
+
+
+		//middle = (0 + 1) / 2 = 0
 	}//end while
+	
+	//cout << "Comparison count: " << comparisonCount << endl; 
+
 	if (found)
 	{
 		return middle;
@@ -69,14 +105,28 @@ int main()
 	//targetValue = 1; //at "front" (pedantic)
 	
 	//targetValue = "cheeseburgers"; //O(N/2)
-	vector<int> sortedNums = { 11, 22, 33, 44, 55, 66, 77, 88 };
+	//vector<int> sortedNums = { 11, 22, 33, 44, 55, 66, 77, 88, 99 };
 
-	int targetValue = 88; 
-	cout << "Found? the element " << targetValue << " at index: " 
-		<< binarySearch(sortedNums, targetValue);
+	//int targetValue = 99; 
+	//cout << "Found? the element " << targetValue << " at index: " 
+	//	<< binarySearch(sortedNums, targetValue);
 
-	//cout << "The element " << targetValue << " was found at index: " 
-	//	<< sequentialSearch(vowels, targetValue);
-	cout << "\n\n";
-	
+	////cout << "The element " << targetValue << " was found at index: " 
+	////	<< sequentialSearch(vowels, targetValue);
+	//cout << "\n\n";
+
+	std::vector<int> primes = { 1, 2, 3, 5, 7, 11, 13, 17, 19, 23 };
+	cout << binarySearch(primes, 13) << endl;
+	//std::vector<int> nums64;
+	//for (int i = 1; i <= 64; i++) {
+	//	nums64.push_back(i); //{1, 2, 3, ...64}
+	//}
+	//std::vector<int> nums1024;
+	//for (int i = 1; i <= 1024; i++) {
+	//	nums1024.push_back(i);
+	//}
+
+	//binarySearch(nums64, 64);
+	//binarySearch(nums1024, 1024);
+
 }
